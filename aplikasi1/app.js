00fde39bf2e001d2f0e7715075f008edcb6cd1f6
CalvinAnglerian <calvinangelriansusanto24@gmail.com>
2025-09-17 13:44:22 +0700

    updatepertemuan1

--- aplikasi1/app.js
+++ aplikasi1/app.js
@@ -10,3 +10,9 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
+
+// cd : buat nambah nama file
+// C:\PAW II SI5C\project-express-js-CalvinAnglerian>
+
+// node : buat menjalankan dalam codingann yang mau di buka
+// misal : node app.js
